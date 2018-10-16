66eea5bd4923877379a0b2df5ed2d110db88885b
yasutomog <yasutomo.goto@gmail.com>
2018-10-16 09:50:34 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  
+  modules: [
+    '@nuxtjs/pwa'
+  ],
+  manifest: {
+    name: 'yasutomog nuxt pwa',
+    lang: 'ja'
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-pwa',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
